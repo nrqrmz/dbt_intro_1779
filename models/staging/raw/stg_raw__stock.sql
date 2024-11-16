f4216701441398700f76c66cffc263f8f0f7f4d6
Enrique Ramirez Ramos <nrqrmz@gmail.com>
2024-11-16 02:27:48 +0000

    add models, descriptions and tests

---
+++ models/staging/raw/stg_raw__stock.sql
@@ -0,0 +1,25 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'stock') }}
+
+),
+
+renamed as (
+
+    select
+        model,
+        model_name,
+        color,
+        color_name,
+        size,
+        forecast_stock,
+        stock,
+        price
+
+    from source
+
+)
+
+select * from renamed
